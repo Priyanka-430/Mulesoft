475ac854d843e6972e85f14e54bcbb9ffe7abb8d
Priyanka-430 <90886446+Priyanka-430@users.noreply.github.com>
2021-11-03 22:09:12 +0530

    Create Movies2.java

---
+++ Movies2.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+public class Movies2 {
+	public static void main(String args[])throws Exception{
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection connection=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","tiger");
+			Statement state=connection.createStatement();
+			String k="select * from moviedetails where leadactor='Prabhas'";
+			ResultSet result=state.executeQuery(k);
+			while(result.next()) {
+				System.out.println("Movie Name : "+result.getString(1));
+				System.out.println("Lead Actor : "+result.getString(2));
+				System.out.println("Actress : "+result.getString(3));
+				System.out.println("Year of Release : "+result.getInt(4));
+				System.out.println("Director : "+result.getString(5));
+			}
+		
+		connection.close();
+		}
+	catch(Exception e) {
+		System.out.println(e);
+	}
+	}
+
+}
